d154014329882e3f1e6434c52d19f1a980026fe1
Reuben Schmidt <reuben.schmidt@icloud.com>
2018-10-05 12:12:57 +1000

    add sidebar

--- client/controllers/osces.js
+++ client/controllers/osces.js
@@ -1,38 +1,41 @@
 var myApp = angular.module('myApp');
 
-myApp.controller('OscesController', ['$scope', '$http', '$location', '$routeParams', function($scope, $http, $location, $routeParams){
-	console.log('OscesController loaded...');
+myApp.controller('OscesController', ['$scope', '$http', '$location', '$routeParams', '$document', function ($scope, $http, $location, $routeParams, $document) {
+    console.log('OscesController loaded...');
 
-	$scope.getOsces = function(){
-		$http.get('/api/osces').success(function(response){
-			$scope.osces = response;
-		});
-	}
+    $scope.rotation = ['Aged Care', 'General Practise', 'Mental Health', 'Obstetrics', 'Paediatrics'];
 
-	$scope.getOsce = function(){
-		var id = $routeParams.id;
-		$http.get('/api/osces/'+id).success(function(response){
-			$scope.osce = response;
-		});
-	}
+    $scope.getOsces = function () {
+        $http.get('/api/osces').success(function (response) {
+            $scope.osces = response;
+        });
+    }
 
-	$scope.addOsce = function(){
-		console.log($scope.osce);
-		$http.post('/api/osces/', $scope.osce).success(function(response){
-			window.location.href='#/osces';
-		});
-	}
+    $scope.getOsce = function () {
+        var id = $routeParams.id;
+        $http.get('/api/osces/' + id).success(function (response) {
+            $scope.osce = response;
+        });
+    }
 
-	$scope.updateOsce = function(){
-		var id = $routeParams.id;
-		$http.put('/api/osces/'+id, $scope.osce).success(function(response){
-			window.location.href='#/osces';
-		});
-	}
+    $scope.addOsce = function () {
+        console.log($scope.osce);
+        $http.post('/api/osces/', $scope.osce).success(function (response) {
+            window.location.href = '#/osces';
+        });
+    }
 
-	$scope.removeOsce = function(id){
-		$http.delete('/api/osces/'+id).success(function(response){
-			window.location.href='#/osces';
-		});
-	}
+    $scope.updateOsce = function () {
+        var id = $routeParams.id;
+        $http.put('/api/osces/' + id, $scope.osce).success(function (response) {
+            window.location.href = '#/osces';
+        });
+    }
+
+    $scope.removeOsce = function (id) {
+        $http.delete('/api/osces/' + id).success(function (response) {
+            window.location.href = '#/osces';
+        });
+    }
+
 }]);
--- models/osce.js
+++ models/osce.js
@@ -2,57 +2,132 @@ const mongoose = require('mongoose');
 
 // Osce Schema
 const osceSchema = mongoose.Schema({
-	title:{
-		type: String,
-		required: true
-	},
-	topic:{
-		type: String,
-		required: true
-	},
-	name:{
-		type: String
-	},
-	age:{
-		type: String,
-	},
-	create_date:{
-		type: Date,
-		default: Date.now
-	}
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+    },
+    age: {
+        type: String,
+    },
+    background: {
+        type: String,
+    },
+    issue: {
+        type: String,
+    },
+    onset: {
+        type: String,
+    },
+    progress: {
+        type: String,
+    },
+    exacerbating: {
+        type: String,
+    },
+    relieving: {
+        type: String,
+    },
+    associated: {
+        type: String,
+    },
+    timing: {
+        type: String,
+    },
+    episodes: {
+        type: String,
+    },
+    severity: {
+        type: String,
+    },
+    systems: {
+        type: String,
+    },
+    pmhx: {
+        type: String,
+    },
+    medications: {
+        type: String,
+    },
+    allergies: {
+        type: String,
+    },
+    fhx: {
+        type: String,
+    },
+    shx: {
+        type: String,
+    },
+    concern: {
+        type: String,
+    },
+    create_date: {
+        type: Date,
+        default: Date.now
+    }
 });
 
 const Osce = module.exports = mongoose.model('Osce', osceSchema);
 
 // Get Osces
 module.exports.getOsces = (callback, limit) => {
-	Osce.find(callback).limit(limit);
+    Osce.find(callback).limit(limit);
 }
 
 // Get Osce
 module.exports.getOsceById = (id, callback) => {
-	Osce.findById(id, callback);
+    Osce.findById(id, callback);
 }
 
 // Add Osce
 module.exports.addOsce = (osce, callback) => {
-	Osce.create(osce, callback);
+    Osce.create(osce, callback);
 }
 
 // Update Osce
 module.exports.updateOsce = (id, osce, options, callback) => {
-	var query = {_id: id};
-	var update = {
-		title: osce.title,
-		topic: osce.topic,
-		name: osce.name,
-		age: osce.age
-	}
-	Osce.findOneAndUpdate(query, update, options, callback);
+    var query = {
+        _id: id
+    };
+    var update = {
+        title: osce.title,
+        category: osce.category,
+        name: osce.name,
+        age: osce.age,
+        background: osce.background,
+        issue: osce.issue,
+        onset: osce.onset,
+        progress: osce.progress,
+        exacerbating: osce.exacerbating,
+        relieving: osce.relieving,
+        associated: osce.associated,
+        timing: osce.timing,
+        episodes: osce.episodes,
+        severity: osce.severity,
+        systems: osce.systems,
+        pmhx: osce.pmhx,
+        medications: osce.medications,
+        allergies: osce.allergies,
+        fhx: osce.fhx,
+        shx: osce.shx,
+        concern: osce.concern
+    }
+    Osce.findOneAndUpdate(query, update, options, callback);
 }
 
 // Delete Osce
 module.exports.removeOsce = (id, callback) => {
-	var query = {_id: id};
-	Osce.remove(query, callback);
+    var query = {
+        _id: id
+    };
+    Osce.remove(query, callback);
 }
